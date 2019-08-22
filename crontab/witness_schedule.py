c1702100df5e945ed3ffad336fc28c08e9646962
sun haoyu <shydesky@gmail.com>
2019-08-22 18:09:17 +0800

    witness schedule

--- crontab/witness_schedule.py
+++ crontab/witness_schedule.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     try:
         current_time = datetime.datetime.now()
-        witness_service.make_witness_schedule(current_time)
+        witness_service.make_witness_schedule("make_witness_schedule", current_time)
         logging.info('execute crontab [%s] success.', taskname)
     except Exception as e:
         logging.error('execute crontab [%s] error: %s', taskname, e)
