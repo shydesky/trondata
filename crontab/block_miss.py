1758832df9784225871c3a9059811c2dfbcb2984
sun haoyu <shydesky@gmail.com>
2019-08-23 12:01:47 +0800

    crontab add params

--- crontab/block_miss.py
+++ crontab/block_miss.py
@@ -1,15 +1,19 @@
 import logging
+import sys
 import datetime
 from backend.block_product.block_miss import BlockMiss
 from autoapp import app
 
 taskname = "block miss"
 if __name__ == "__main__":
-
     try:
-        current_time = datetime.datetime.now() - datetime.timedelta(days=1)
+        if len(sys.argv) > 1:
+            time_str = sys.argv[1]
+            utc_date_time = datetime.datetime.strptime(time_str, "%Y-%m-%d")
+        else:
+            utc_date_time = datetime.datetime.now() - datetime.timedelta(days=1)
         with app.app_context():
-            miss = BlockMiss(current_time)
+            miss = BlockMiss(utc_date_time)
             miss.calc()
             miss.format()
             logging.info('execute crontab [%s] success. Date: %s', taskname, miss.utc_date_time)
