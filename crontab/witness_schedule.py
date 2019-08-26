861d90206ddcb4482a1a880562a3145103d6141d
sun haoyu <shydesky@gmail.com>
2019-08-26 14:00:52 +0800

    params

--- crontab/witness_schedule.py
+++ crontab/witness_schedule.py
@@ -9,8 +9,12 @@ taskname = "witness schedule"
 if __name__ == "__main__":
 
     try:
-        current_time = datetime.datetime.now() - datetime.timedelta(days=1)
-        witness_service.witness_services("make_witness_schedule", current_time)
+        if len(sys.argv) > 1:
+            time_str = sys.argv[1]
+            utc_date_time = datetime.datetime.strptime(time_str, "%Y-%m-%d")
+        else:
+            utc_date_time = datetime.datetime.now() - datetime.timedelta(days=1)
+        witness_service.witness_services("make_witness_schedule", utc_date_time)
         logging.info('execute crontab [%s] success.', taskname)
     except Exception as e:
         logging.error('execute crontab [%s] error: %s', taskname, e)
