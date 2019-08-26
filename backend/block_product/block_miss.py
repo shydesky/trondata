54b4633d7b7a121dfacc0c80f8e8d486909425c8
sun haoyu <shydesky@gmail.com>
2019-08-26 14:57:10 +0800

    log maintaince

--- backend/block_product/block_miss.py
+++ backend/block_product/block_miss.py
@@ -31,7 +31,7 @@ def calc_maintaince_slot_timestamp(utc_date_time):
         maintaince_slot_timestamp.add(item[1] - 3)
         maintaince_slot_timestamp.add(item[1] - 6)
 
-    logging.info("maintaince slot_timestamps are %s", ",".join(map(lambda x: str(x), list(maintaince_slot_timestamp).sort()))
+    logging.info("maintaince slot_timestamps are %s", ",".join(map(lambda x: str(x), list(maintaince_slot_timestamp).sort())))
     return maintaince_slot_timestamp
 
 def calc_missblock_timestamp_height(timestamp_list=None):
